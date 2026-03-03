7e88715a78a9d9cd81ebfc323c59ba4b4c0fd72b
tt05vathan <tt05@godhaar.com>
2026-03-03 12:10:25 +0530

    updated

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -1,9 +1,6 @@
-import { createClient } from '@vercel/kv';
+import { kv } from '@vercel/kv';
 
-export const kv = createClient({
-    url: process.env.KV_REST_API_URL!,
-    token: process.env.KV_REST_API_TOKEN!,
-});
+export { kv };
 
 export type Customer = {
     id: string;
